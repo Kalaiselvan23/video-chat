408a9e956df22c9db2f9f64225f83ac460a34717
kalaiselvan23 <kalaiselvan1623@gmail.com>
2024-06-21 15:41:03 +0530

    added webrtc logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import ws, { WebSocketServer } from "ws";
 import WebSocket from "ws";
 import express from "express";
-import { Server } from "socket.io";
 import bodyParser from "body-parser";
 import authRouter from "./routes/auth";
 import cors from "cors";
@@ -13,74 +12,159 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use("/auth", authRouter);
 const server = app.listen(port, () => {
-  console.log(`Listening in port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 const wss = new WebSocketServer({ server: server });
 const users = new Map();
 const rooms = new Map();
+let senderSocket: WebSocket | null = null;
+let recieverSocket: WebSocket | null = null;
+
 wss.on("connection", (ws) => {
   const userId = `user-${Math.random().toString(36).substring(2, 15)}`;
   users.set(userId, ws);
+
   ws.on("error", console.error);
-  console.log(users);
-  ws.on("message", (data, isBinary) => {
+
+  ws.on("message", (data) => {
     const message = JSON.parse(data.toString());
-    if ((message.type = "create")) {
-      const userId = message.userId;
+
+    if (message.type === "create") {
       const roomId = `room-${Math.random().toString(36).substring(2, 15)}`;
       rooms.set(roomId, new Set());
       rooms.get(roomId).add(userId);
+      console.log(`Room created with ID ${roomId} and user ${userId} joined`);
+      senderSocket = ws;
       ws.send(
         JSON.stringify({
-          type: "info",
+          type: "create-response",
+          roomId: roomId,
           message: `Room created with ID ${roomId} and you were joined`,
         })
       );
-    } else if (message.type == "join") {
+    } else if (message.type === "join") {
       const roomId = message.roomId;
-      const userId = message.userId;
       if (!rooms.has(roomId)) {
-        JSON.stringify({ type: "error", message: "No room in this roomId" });
+        ws.send(
+          JSON.stringify({
+            type: "error",
+            message: "No room with this roomId exists",
+          })
+        );
+      } else {
+        rooms.get(roomId).add(userId);
+        recieverSocket = ws;
+        console.log(`User ${userId} joined room ${roomId}`);
+        ws.send(
+          JSON.stringify({
+            type: "join-response",
+            message: `Joined room ${roomId}`,
+            roomId: roomId,
+          })
+        );
       }
-      rooms.get(roomId).add(userId);
-      ws.send(
-        JSON.stringify({ type: "info", message: `Joined room ${roomId}` })
-      );
-    } else if (message.type == "message") {
+    } else if (message.type === "message") {
       const roomId = message.roomId;
       if (rooms.has(roomId)) {
-        users.forEach((uuid) => {
-          const client = users.get(uuid);
-          if (client && client.readyState === WebSocket.OPEN) {
+        users.forEach((client, uuid) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
             client.send(
               JSON.stringify({
                 type: "message",
                 message: message.content,
-              }),
-              { binary: isBinary }
+              })
             );
           }
         });
       }
+    } else if (message.type === "fetchUsers") {
+      const roomId = message.roomId;
+      if (rooms.has(roomId)) {
+        users.forEach((client, uuid) => {
+          client.send(
+            JSON.stringify({
+              type: "roomUsers-response",
+              users: Array.from(rooms.get(roomId)),
+            })
+          );
+        });
+        // const roomUsers = Array.from(rooms.get(roomId));
+        // console.log(roomUsers)
+        // ws.send(
+        //   JSON.stringify({
+        //     type: "roomUsers-response",
+        //     users: roomUsers,
+        //   })
+        // );
+        // wss.clients.forEach((client) => {});
+      } else {
+        ws.send(
+          JSON.stringify({
+            type: "room-error",
+            msg: "room not found",
+          })
+        );
+      }
+    } else if (message.type === "createOffer") {
+      if (ws !== senderSocket) {
+        return;
+      }
+      recieverSocket?.send(
+        JSON.stringify({ type: "createOffer", sdp: message.sdp })
+      );
+    } else if (message.type === "createAnswer") {
+      if (ws !== recieverSocket) {
+        return;
+      }
+      senderSocket?.send(
+        JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+      );
+    } else if (message.type === "iceCandidate") {
+      if (ws === senderSocket) {
+        recieverSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      } else if (ws === recieverSocket) {
+        senderSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      }
     }
   });
-  ws.on("close", () => {
-    console.log(`Connection closed: ${userId}`);
-    users.delete(userId);
 
-    rooms.forEach((roomUsers, roomId) => {
-      if (roomUsers.has(userId)) {
-        roomUsers.delete(userId);
-        if (roomUsers.size === 0) {
-          rooms.delete(roomId);
+  ws.on('close', () => {
+    const userRoomId = users.get(userId).roomId;
+    if (userRoomId && rooms.get(userRoomId)) {
+        rooms.get(userRoomId).users = rooms.get(userRoomId).users.filter((uid:any) => uid !== userId);
+        if (rooms.get(userRoomId).users.length === 0) {
+            rooms.delete(userRoomId);
+        } else {
+            broadCastUserList(userRoomId);
         }
-      }
-    });
-  });
+    }
+     users.delete(userId);
+});
+
   ws.send(
     JSON.stringify({
-      type: "info",
-      message: `Hello from backend server. Your ID is ${userId}`,
+      type: "id-info",
+      userId,
+      message: `Hello from the backend server. Your ID is ${userId}`,
     })
   );
 });
+
+const broadCastUserList = (roomId: string) => {
+  if (rooms.get(roomId)) {
+    users.forEach((client, uuid) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({
+            type: "roomUsers-response",
+            users: Array.from(rooms.get(roomId)),
+          })
+        );
+      }
+    });
+  }
+};
